96dd042adfd00f53005590e3ccb62d7600a075b8
marrysoegino <ms8169@nyu.edu>
2016-12-08 09:10:42 -0500

    no message

---
+++ CookerHandsUltra/Assets/scripts/instruction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class instruction : MonoBehaviour {
+    public float actualTime;
+    public float gameTime;
+
+    // Use this for initialization
+    void Start () {
+        gameTime = 120.0f;
+        actualTime = gameTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        actualTime -= Time.deltaTime;
+        
+        if (actualTime <= 0.0f){
+            
+            Destroy(GameObject.Find("Canvas"));
+        }
+
+
+    }
+}
